6bca21b423ae3768f0be566ed445705ba88d9011
Sebastian Springer <info@sebastian-springer.com>
2018-10-10 11:18:03 +0200

    write

--- fs.js
+++ fs.js
@@ -6,3 +6,10 @@ fs.readFile('input.txt', 'utf-8', (err, data) => {
   }
   console.log(data);
 });
+
+fs.appendFile('input.txt', 'bye bye', err => {
+  if (err) {
+    console.error(err);
+  }
+  console.log('done');
+});
