8430d18ac088d59896b60e41e5eb0c4dd400c424
Sebastian Springer <info@sebastian-springer.com>
2018-10-10 15:03:42 +0200

    more templates

--- web/todo/controller.js
+++ web/todo/controller.js
@@ -4,8 +4,9 @@ const model = require('./model');
 module.exports = async (req, res) => {
   try {
     const data = await model.getItems();
-    res.send(render(data));
+    res.send(await render(data));
   } catch (e) {
+    console.log(e);
     res.sendStatus(500);
   }
 };
--- web/todo/view.js
+++ web/todo/view.js
@@ -1,2 +1,9 @@
-module.exports = data =>
-  `<ul>${data.map(todo => `<li>${todo.title}</li>`).join('')}</ul>`;
+const fs = require('fs').promises;
+
+module.exports = async data => {
+  const template = await fs.readFile('./todo/list.html', 'utf-8');
+  return template.replace(
+    '//list//',
+    `<ul>${data.map(todo => `<li>${todo.title}</li>`).join('')}</ul>`,
+  );
+};
