a33a42cf4dbee13fd98cd20f723622240cc45bcd
Sebastian Springer <info@sebastian-springer.com>
2018-10-10 11:31:37 +0200

    more stuff

--- fs.promise.js
+++ fs.promise.js
@@ -8,6 +8,9 @@ fs.readFile('input.txt', 'utf-8')
   try {
     const data = await fs.readFile('input.txt', 'utf-8');
     console.log('async: ', data);
+    await fs.writeFile('input.txt', 'noch mehr bla bla');
+    const data2 = await fs.readFile('input.txt', 'utf-8');
+    console.log('async: ', data2);
   } catch (e) {
     console.error(e);
   }
