24357b817bed07ea602230421d5b33719c6b5eb6
Sebastian Springer <info@sebastian-springer.com>
2018-10-10 14:27:13 +0200

    morgan middleware

--- web/index.js
+++ web/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 
 // npm install morgan
+
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, 'access.log'),
+  { flags: 'a' },
+);
+app.use(morgan('combined', { stream: accessLogStream }));
 
 app.use((req, res, next) => {
   console.log('I am your middleware');
